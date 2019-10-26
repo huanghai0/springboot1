865a873b440a153cfe075aeac6b7ffae988d20e8
Eruda <1161766681@qq.com>
2019-10-26 10:21:10 +0800

    Initial commit

---
+++ src/main/java/com/bee/sample/ch1/entity/UserBean.java
@@ -0,0 +1,44 @@
+package com.bee.sample.ch1.entity;
+
+
+import org.beetl.sql.core.annotatoin.Table;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
+
+//@Entity
+@Table(name = "user")
+public class UserBean {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//
+//    private int id;
+//    private String name;
+//    private String password;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+}
